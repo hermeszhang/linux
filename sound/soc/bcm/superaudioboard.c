24fa23fb5c12382d390e45aaebfac967e13b4c5f
William Hollender <whollender@gmail.com>
2015-06-29 18:44:46 -0700

    Modified device tree compatible string to "superaudio,superaudioboard" to more closely match other ones and added board level driver under sound/soc/bcm.  Do some checking in cs4271 driver to make sure it should work, but should be ready to test.

---
+++ sound/soc/bcm/superaudioboard.c
@@ -0,0 +1,142 @@
+/*
+ * ASoC Driver for SuperAudioBoard
+ *
+ * Author:	R F William Hollender <whollender@gmail.com>
+ *		Copyright 2015
+ *		based on code by Daniel Matuschek <daniel@hifiberry.com>
+ *		and Florian Meier <florian.meier@koalo.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+#include <linux/module.h>
+#include <linux/platform_device.h>
+
+#include <sound/core.h>
+#include <sound/pcm.h>
+#include <sound/pcm_params.h>
+#include <sound/soc.h>
+#include <sound/jack.h>
+
+//#include "../codecs/pcm512x.h"
+
+static int snd_rpi_superaudioboard_init(struct snd_soc_pcm_runtime *rtd)
+{
+	struct snd_soc_codec *codec = rtd->codec;
+	// Don't think any init code to put here
+	return 0;
+}
+
+static int snd_rpi_superaudioboard_hw_params(struct snd_pcm_substream *substream,
+				       struct snd_pcm_hw_params *params)
+{
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_dai *cpu_dai = rtd->cpu_dai;
+
+	// TODO: Need to set this to a different value or read in value (like hifiberry-dac)?
+	// Note, the bclk ratio is always 64 in master mode. Need to double check CS4271 driver
+	// to see if it uses correct mode.
+	return snd_soc_dai_set_bclk_ratio(cpu_dai, 64);
+}
+
+static int snd_rpi_superaudioboard_startup(struct snd_pcm_substream *substream) {
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_codec *codec = rtd->codec;
+	return 0;
+}
+
+static void snd_rpi_superaudioboard_shutdown(struct snd_pcm_substream *substream) {
+	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_codec *codec = rtd->codec;
+}
+
+/* machine stream operations */
+static struct snd_soc_ops snd_rpi_superaudioboard_ops = {
+	.hw_params = snd_rpi_superaudioboard_hw_params,
+	.startup = snd_rpi_superaudioboard_startup,
+	.shutdown = snd_rpi_superaudioboard_shutdown,
+};
+
+static struct snd_soc_dai_link snd_rpi_superaudioboard_dai[] = {
+{
+	.name		= "SuperAudioBoard",
+	.stream_name	= "SuperAudioBoard HiFi",
+	.cpu_dai_name	= "bcm2708-i2s.0",
+	.codec_dai_name	= "cs4271-hifi",
+	.platform_name	= "bcm2708-i2s.0",
+	.codec_name	= "cs4271.1-0010", // TODO: This seems to be codec name . i2c # (1) - i2c address, but not sure
+	.dai_fmt	= SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF | // TODO: Need to check these settings
+				SND_SOC_DAIFMT_CBS_CFS,
+	.ops		= &snd_rpi_superaudioboard_ops,
+	.init		= snd_rpi_superaudioboard_init,
+},
+};
+
+/* audio machine driver */
+static struct snd_soc_card snd_rpi_superaudioboard = {
+	.name         = "snd_rpi_superaudioboard",
+	.dai_link     = snd_rpi_superaudioboard_dai,
+	.num_links    = ARRAY_SIZE(snd_rpi_superaudioboard_dai),
+};
+
+static int snd_rpi_superaudioboard_probe(struct platform_device *pdev)
+{
+	int ret = 0;
+
+	snd_rpi_superaudioboard.dev = &pdev->dev;
+
+	if (pdev->dev.of_node) {
+	    struct device_node *i2s_node;
+	    struct snd_soc_dai_link *dai = &snd_rpi_superaudioboard_dai[0];
+	    i2s_node = of_parse_phandle(pdev->dev.of_node,
+					"i2s-controller", 0);
+
+	    if (i2s_node) {
+		dai->cpu_dai_name = NULL;
+		dai->cpu_of_node = i2s_node;
+		dai->platform_name = NULL;
+		dai->platform_of_node = i2s_node;
+	    }
+	}
+
+	ret = snd_soc_register_card(&snd_rpi_superaudioboard);
+	if (ret)
+		dev_err(&pdev->dev,
+			"snd_soc_register_card() failed: %d\n", ret);
+
+	return ret;
+}
+
+static int snd_rpi_superaudioboard_remove(struct platform_device *pdev)
+{
+	return snd_soc_unregister_card(&snd_rpi_superaudioboard);
+}
+
+static const struct of_device_id snd_rpi_superaudioboard_of_match[] = {
+	{ .compatible = "superaudio,superaudioboard", },
+	{},
+};
+MODULE_DEVICE_TABLE(of, snd_rpi_superaudioboard_of_match);
+
+static struct platform_driver snd_rpi_superaudioboard_driver = {
+	.driver = {
+		.name   = "snd-rpi-superaudioboard",
+		.owner  = THIS_MODULE,
+		.of_match_table = snd_rpi_superaudioboard_of_match,
+	},
+	.probe          = snd_rpi_superaudioboard_probe,
+	.remove         = snd_rpi_superaudioboard_remove,
+};
+
+module_platform_driver(snd_rpi_superaudioboard_driver);
+
+MODULE_AUTHOR("R F William Hollender <whollender@gmail.com>");
+MODULE_DESCRIPTION("ASoC Driver for SuperAudioBoard");
+MODULE_LICENSE("GPL v2");
